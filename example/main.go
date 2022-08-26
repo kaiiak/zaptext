6d0baa367207f7556b20f33cb20e127f4f848548
kaiiak <aNxFi37X@outlook.com>
2022-08-26 21:32:50 +0800

    example use text encoder

--- example/main.go
+++ example/main.go
@@ -1,12 +1,22 @@
 package main
 
-import "go.uber.org/zap"
+import (
+	"os"
+
+	"github.com/kaiiak/zaptext"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 func main() {
-	logger, err := zap.NewDevelopment()
-	if err != nil {
-		panic(err)
-	}
-	defer logger.Sync()
+	var (
+		logger *zap.Logger
+		cfg    = zap.NewProductionEncoderConfig()
+		te     = zaptext.NewTextEncoder(cfg)
+	)
+
+	logger = zap.New(zapcore.NewCore(te, os.Stdout, zap.DebugLevel))
+	zap.ReplaceGlobals(logger)
+
 	logger.Info("msg", zap.Int64s("ids", []int64{1, 2, 3}))
 }
